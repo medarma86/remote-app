0b280cf48be914322b87f8119bd32c86b586538d
Madhu <medarama86@gmail.com>
2025-12-29 19:55:24 +0000

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProfileView } from './profile/profile-view/profile-view';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // Matches localhost:4201 exactly
+    pathMatch: 'full',
+    component: Home 
+  },
+    {
+    path: 'profile',
+    component: ProfileView
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive], 
+  templateUrl: './app.html',           
+  styleUrls: ['./app.scss']           
 })
-export class App {
-  protected readonly title = signal('remote-app');
-}
+export class App {}
